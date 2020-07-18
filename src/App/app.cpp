5378ac4674f4136422ef6e98d1d17dfb89ea582d
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-07-18 21:14:57 +0800

    demo test

--- src/App/app.cpp
+++ src/App/app.cpp
@@ -4,17 +4,26 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <Renderer/command.h>
+#include <Renderer/camera.h>
+#include <Renderer/Renderer2D.h>
 
 namespace Chaf
 {
     EditorLayer::EditorLayer()
-        : Layer("EditorLayer")
+        : Layer("EditorLayer"), m_CameraController(23.0f, 16.0f / 9.0f, CameraType::Perspective)
     {
-
+        Renderer2D::Init();
     }
 
     void EditorLayer::OnAttach()
     {
+        m_Texture = Texture2D::Create("assets/texture/test.png");
+
+        FrameBufferSpecification fbSpec;
+        fbSpec.Width = 1280;
+        fbSpec.Height = 720;
+        m_FrameBuffer = FrameBuffer::Create(fbSpec);
     }
 
     void EditorLayer::OnDetach()
@@ -24,6 +33,21 @@ namespace Chaf
 
     void EditorLayer::OnUpdate(Timestep timestep)
     {
+        //	Update
+        m_CameraController.OnUpdate(timestep);
+
+        //	Render
+
+        m_FrameBuffer->Bind();
+        RenderCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
+        RenderCommand::Clear();
+        Renderer2D::BeginScene(m_CameraController.GetCamera());
+        //Renderer2D::SetLineMode(true);
+        Renderer2D::DrawQuad({ -1.0f,0.0f }, { 0.8f,0.8f }, { 0.8f,0.2f,0.3f,1.0f });
+        Renderer2D::DrawQuad({ 0.5f,-0.5f }, { 0.5f,0.75f }, { 0.2f,0.3f,0.8f,1.0f });
+        Renderer2D::DrawQuad({ 0.0f,0.0f,-0.1f }, { 10.0f,10.0f }, m_Texture, { 0.2f,0.8f,0.3f,1.0f });
+        Renderer2D::EndScene();
+        m_FrameBuffer->Unbind();
     }
 
     void EditorLayer::OnImGuiRender()
@@ -31,24 +55,35 @@ namespace Chaf
        
 
         ImGui::Begin("Settings");
-        ImGui::Text("Hello World");
+        ImGui::ColorEdit3("color", glm::value_ptr(m_SquareColor));
         ImGui::End();
 
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0,0 });
         ImGui::Begin("Viewport");
         ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
         //Application::Get().GetImGuiLayer()->SetBlockEvents(!ImGui::IsWindowFocused());
+        m_CameraController.SetActive(ImGui::IsWindowFocused());
+        if (m_ViewportSize != *((glm::vec2*) & viewportPanelSize))
+        {
+            m_ViewportSize = { viewportPanelSize.x,viewportPanelSize.y };
+            m_FrameBuffer->Resize(m_ViewportSize.x, m_ViewportSize.y);
+
+            m_CameraController.OnResize(m_ViewportSize.x, m_ViewportSize.y);
+        }
+
+        uint32_t textureID = m_FrameBuffer->GetColorAttachmentRendererID();
+        ImGui::Image((void*)textureID, ImVec2(m_ViewportSize.x, m_ViewportSize.y), ImVec2(0, 1), ImVec2(1, 0));
         ImGui::End();
         ImGui::PopStyleVar();
 
         static bool show = true;
         ImGui::ShowDemoWindow(&show);
-
        
 
     }
 
     void EditorLayer::OnEvent(Event& event)
     {
+        m_CameraController.OnEvent(event);
     }
 }
--- src/App/app.h
+++ src/App/app.h
@@ -2,6 +2,10 @@
 
 #include <Engine/layer.h>
 #include <Engine/time.h>
+#include <Renderer/buffer.h>
+#include <Renderer/texture.h>
+#include <Renderer/vao.h>
+#include <Editor/camera_controller.h>
 
 namespace Chaf
 {
@@ -17,5 +21,16 @@ namespace Chaf
 		virtual void OnUpdate(Timestep timestep) override;
 		virtual void OnImGuiRender() override;
 		virtual void OnEvent(Event& event) override;
+	private:
+		CameraController m_CameraController;
+
+		//	Temp
+		Ref<FrameBuffer> m_FrameBuffer;
+		Ref<VertexArray> m_VertexArray;
+		glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.4f };
+
+		glm::vec2 m_ViewportSize = { 0.0f, 0.0f };
+
+		Ref<Texture2D> m_Texture;
 	};
 }
