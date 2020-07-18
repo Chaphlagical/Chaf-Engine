886ce0a4518a6c018ce54c58e020bbeeaa2e4ee6
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-07-18 12:33:49 +0800

    update application

--- src/Editor/application.cpp
+++ src/Editor/application.cpp
@@ -1,24 +1,103 @@
 #include <Editor/application.h>
 #include <Engine/log.h>
 
+#include <Renderer/renderer.h>
+
 namespace Chaf
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x ,this ,std::placeholders::_1)
+
+	Application* Application::s_Instance = nullptr;
+
 	Application::Application(const std::string name)
 	{
+		CHAF_CORE_ASSERT(!s_Instance, "Application already exists!");
+		s_Instance = this;
 
+		m_Window = Ref<Window>(Window::Create(name));
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+
+		Renderer::Init();
+
+		m_ImGuiLayer = new ImGuiLayer(m_Window);
+		PushOverlay(m_ImGuiLayer);
 	}
 
 	Application::~Application()
 	{
+		Renderer::Shutdown();
+	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
+
+		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin();)
+		{
+			(*--it)->OnEvent(e);
+			if (e.m_Handled)
+				break;
+		}
+	}
+
+	void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
+	}
+
+	void Application::PushOverlay(Layer* layer)
+	{
+		m_LayerStack.PushOverlay(layer);
+		layer->OnAttach();
+	}
+
+	void Application::Close()
+	{
+		m_Running = false;
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
 	}
 
 	void Application::Run()
 	{
 		while (m_Running)
 		{
-			CHAF_INFO("App running!");
+			Timestep timestep;
+			timestep.Update(m_LastFrameTime);
+			m_LastFrameTime = timestep.GetSeconds();
+
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+				m_ImGuiLayer->Begin();
+				{
+					for (Layer* layer : m_LayerStack)
+						layer->OnImGuiRender();
+				}
+				m_ImGuiLayer->End();
+			}
+			m_Window->OnUpdate();
 		}
 	}
-
 }
--- src/Editor/application.h
+++ src/Editor/application.h
@@ -3,6 +3,10 @@
 #include <string>
 
 #include <Engine/core.h>
+#include <Engine/layer.h>
+#include <Renderer/window.h>
+#include <Gui/imgui_layer.h>
+
 
 namespace Chaf
 {
@@ -13,8 +17,27 @@ namespace Chaf
 		virtual ~Application();
 
 		void Run();
+		void OnEvent(Event& e);
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
+		void Close();
+	public:
+		inline static Application& Get() { return *s_Instance; }
+		inline Window& GetWindow() { return *m_Window; }
+		ImGuiLayer* GetImGuiLayer() { return m_ImGuiLayer; }
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+		bool OnWindowResize(WindowResizeEvent& e);
 	private:
 		bool m_Running = true;
+		bool m_Minimized = false;
+		float m_LastFrameTime = 0.0f;
+	private:
+		Ref<Window> m_Window;
+		ImGuiLayer* m_ImGuiLayer;
+		LayerStack m_LayerStack;
+	private:
+		static Application* s_Instance;
 	};
 
 	//	implement in client
