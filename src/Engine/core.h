addddd21a2108552315e6d526a0028e38f82979f
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-07-22 11:16:32 +0800

    update

--- src/Engine/core.h
+++ src/Engine/core.h
@@ -49,7 +49,7 @@ namespace Chaf
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
 	template<typename T, typename ... Args>
-	constexpr Scope<T> CreateScrop(Args&& ... args)
+	constexpr Scope<T> CreateScope(Args&& ... args)
 	{
 		return std::make_unique<T>(std::forward<Args>(args)...);
 	}
