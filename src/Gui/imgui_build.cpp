9c18f7881fdd4c2b989ee3f4ecbd9184b15a1799
Wenbo Chen <chaf@mail.ustc.edu.cn>
2021-09-16 20:36:20 +0800

    update

--- src/Gui/imgui_build.cpp
+++ src/Gui/imgui_build.cpp
@@ -1,5 +1,5 @@
 #ifdef CHAF_OPENGL_API
 #define IMGUI_IMPL_OPENGL_LOADER_GLAD
-#include <examples/imgui_impl_opengl3.cpp>
-#include <examples/imgui_impl_glfw.cpp>
+#include <backends/imgui_impl_opengl3.cpp>
+#include <backends/imgui_impl_glfw.cpp>
 #endif // CHAF_OPENGL_API
